b0731f827d1e18806f1e28bf7a13afa795b842bf
Emanuel <101946589+mano3queijos@users.noreply.github.com>
2023-05-19 04:29:08 -0300

    Ex05

--- atividades-pilhas/src/br/com/emanuelLap/pilha/exercicios/Ex05.java
+++ atividades-pilhas/src/br/com/emanuelLap/pilha/exercicios/Ex05.java
@@ -1,7 +1,32 @@
 package br.com.emanuelLap.pilha.exercicios;
 
+import java.util.Stack;
+
+@SuppressWarnings({ "rawtypes", "unchecked" })
+
 public class Ex05 {
 
+	static Stack stack = new Stack();
 	
-	
+	public static void main(String[] args) {
+
+		String word= "ADA";
+		String word2 = "socorrammesubinoonibusemMarrocos";
+		System.out.println("word" + "is palindrome?"+ palindromeTest(word2) );
+		
+	}
+
+	public static boolean palindromeTest(String word) {
+
+		for (int i = 0; i < word.length(); i++) {
+			stack.push(word.charAt(i));
+		}
+		
+		String inverseWord ="";
+		while (!stack.isEmpty()) {
+			inverseWord += stack.pop();
+			
+		}
+		return(word.equalsIgnoreCase(inverseWord)? true: false);
+	}
 }
