a2d2d2538b7eece9921149a3384840cb823fefa9
Emanuel <101946589+mano3queijos@users.noreply.github.com>
2023-05-19 15:14:52 -0300

    Ex06

---
+++ atividades-pilhas/src/br/com/emanuelLap/pilha/exercicios/Ex06.java
@@ -0,0 +1,44 @@
+package br.com.emanuelLap.pilha.exercicios;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+@SuppressWarnings({ "rawtypes", "unchecked" })
+
+public class Ex06 {
+
+	static Stack<Character> stack = new Stack();
+	static Scanner sacn = new Scanner(System.in);
+	final static String ABRE = "([{";
+	final static String FECHA = ")]}";
+
+	public static boolean checkBalancedSymbol(String expression) {
+
+		boolean balanceado = true;
+		char simbolo, topo;
+		for (int i = 0; i < expression.length(); i++) {
+
+			simbolo = expression.charAt(i);
+			if (ABRE.indexOf(simbolo)>-1) {
+				stack.push(simbolo);
+			}
+			else if(FECHA.indexOf(simbolo) > -1) {
+				if(stack.isEmpty()) {
+					return false;
+				}else {
+					topo= stack.pop();
+				}
+				if(ABRE.indexOf(topo) != FECHA.indexOf(simbolo)) {
+					return false;
+				}
+			}
+		}
+
+		while (!stack.isEmpty()) {
+
+		}
+
+		return true;
+	}
+
+}
